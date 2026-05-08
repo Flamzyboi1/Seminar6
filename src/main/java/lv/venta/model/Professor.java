255613a39d9aecaacaecfddde54a9534f404625d
Favour <favourobidiaso@gmail.com>
2026-05-08 16:02:01 +0300

    created course professor and degree classes with setters getters variable names and constroctors as well as to string

--- src/main/java/lv/venta/model/Professor.java
+++ src/main/java/lv/venta/model/Professor.java
@@ -1,10 +1,13 @@
 package lv.venta.model;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -12,24 +15,36 @@ import jakarta.validation.constraints.Pattern;
 
 @Table(name = "ProfessorTAble")
 @Entity
+
+
 public class Professor {
 	@Column(name = "Pid")
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int pid;
+	
+	
 	@Column(name = "Name")
 	@NotNull
 	@NotEmpty
 	@Pattern(regexp = "[A_Z]{1}[a-z]{2,40}")
 	private String name;
+	
+	
 	@Column(name = "Surname")
 	@NotNull
 	@NotEmpty
 	@Pattern(regexp = "[A_Z]{1}[a-z]{2,40}")
 	private String surname;
+	
+	
 	@NotNull
     @Column()
 	private Degree degree;
+	
+	
+	@OneToMany(mappedBy = "Professor")//one professor has many courses,mapped by - use course class professor variable name
+	private Collection<Course>courses;
 	//Setters and Getters
 	public String getName() {
 		return name;
